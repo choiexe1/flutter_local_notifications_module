a5b06548ad2e647f1cbf2d62d1ee22d817f5437e
Junsung Choi <choiexe1@gmail.com>
2025-06-19 20:08:33 +0900

    feat: use case di

--- core/local_push_notifications_module.dart
+++ core/local_push_notifications_module.dart
@@ -1,20 +1,27 @@
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:get_it/get_it.dart';
 
-import '../data/flutter_local_push_notification_repository.dart';
-import '../domain/android_push_config.dart';
-import '../domain/local_push_config.dart';
-import '../domain/local_push_repository.dart';
+import '../data/repositories/flutter_local_push_notification_repository.dart';
+import '../domain/configs/android_push_config.dart';
+import '../domain/configs/local_push_config.dart';
+import '../domain/repositories/local_push_repository.dart';
+import '../domain/use_cases/immediate_push_use_case.dart';
 
 class LocalPushNotificationsModule {
   static void init(GetIt sl) {
     sl
+      // Configs
       ..registerSingleton<LocalPushConfig>(AndroidPushConfig())
       ..registerSingleton<FlutterLocalNotificationsPlugin>(
         FlutterLocalNotificationsPlugin(),
       )
+      // Repositories
       ..registerLazySingleton<LocalPushRepository>(
         () => FlutterLocalPushNotificationRepository(sl(), sl()),
+      )
+      // Use Cases
+      ..registerLazySingleton<ImmediatePushUseCase>(
+        () => ImmediatePushUseCase(sl()),
       );
   }
 }
