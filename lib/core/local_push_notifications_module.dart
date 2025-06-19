ea9e80815b5b33dc5d8c791e1ff2ffd8cff2e166
Junsung Choi <choiexe1@gmail.com>
2025-06-19 21:54:21 +0900

    feat: scheduled push use case di

--- lib/core/local_push_notifications_module.dart
+++ lib/core/local_push_notifications_module.dart
@@ -6,6 +6,7 @@ import 'package:local_push_notifications_module/domain/configs/android_push_conf
 import 'package:local_push_notifications_module/domain/configs/local_push_config.dart';
 import 'package:local_push_notifications_module/domain/repositories/local_push_repository.dart';
 import 'package:local_push_notifications_module/domain/use_cases/immediate_push_use_case.dart';
+import 'package:local_push_notifications_module/domain/use_cases/scheduled_push_use_case.dart';
 
 class LocalPushNotificationsModule {
   static void init(GetIt sl) {
@@ -22,6 +23,9 @@ class LocalPushNotificationsModule {
       // Use Cases
       ..registerLazySingleton<ImmediatePushUseCase>(
         () => ImmediatePushUseCase(sl()),
+      )
+      ..registerLazySingleton<ScheduledPushUseCase>(
+        () => ScheduledPushUseCase(sl()),
       );
   }
 }
